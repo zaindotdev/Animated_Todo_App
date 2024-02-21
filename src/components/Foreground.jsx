502c7fe457e38452613f714d83df4c3e9d36d344
Muhammad Zain <114417655+Zain180@users.noreply.github.com>
2024-02-21 17:15:29 +0500

    foreground changes

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,14 +1,15 @@
 import { useRef, useState } from "react";
 import Card from "./Card";
 import Form from "./Form";
+import { IoMdAdd, IoMdClose } from "react-icons/io";
 function Foreground() {
   const ref = useRef();
-  const [openClose, setOpenClose] = useState("Open Form");
+  const [openClose, setOpenClose] = useState(<IoMdAdd />);
   const [toggle, setToggle] = useState(false);
 
   const handleForm = () => {
     setToggle((prevToggle) => !prevToggle);
-    setOpenClose(toggle ? "Open Form" : "Close Form");
+    setOpenClose(toggle ? <IoMdAdd /> : <IoMdClose />);
   };
   return (
     <>
@@ -18,13 +19,15 @@ function Foreground() {
       >
         <button
           onClick={handleForm}
-          className="bg-zinc-500 absolute top-10 left-10 px-5 py-2 font-bold text-xl text-zinc-100 rounded-md shadow-inner"
+          className="bg-zinc-500 absolute bottom-10 left-10 p-2  font-bold text-3xl text-zinc-100 rounded-full shadow-inner"
           type="button"
         >
           {openClose}
         </button>
         {toggle ? <Form /> : ""}
-        <Card reference={ref} />
+        <div className="flex flex-wrap">
+          <Card reference={ref} />
+        </div>
       </div>
     </>
   );
